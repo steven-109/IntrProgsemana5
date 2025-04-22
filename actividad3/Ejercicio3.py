8d25e94af31dfdd169d3c4b146e2854849aa4ca2
steven-109 <somarenco@uamv.edu.ni>
2025-04-22 08:29:25 -0600

    Ejercicio 3

--- actividad3/Ejercicio3.py
+++ actividad3/Ejercicio3.py
@@ -3,4 +3,12 @@ interes_anual = float(input("Ingrese la cantidad del interes anual es de:"))
 cantidad_años = float(input("Ingrese la cantidad de años es de:"))
 
 porc_dec = interes_anual / 100
-calculo = (1 + interes_anual)
+calculo = (1 + interes_anual)^cantidad_años
+monto_final = calculo * capital_inicial
+interes_ganado = capital_inicial - monto_final
+
+print(f"El capital inicial es de: {capital_inicial}")
+print(f"La tasa de interes anual es de : {interes_anual}")
+print(f"La cantidad de años es de: {cantidad_años}")
+print(f"El monto final es de: {monto_final}")
+print(f"El interes ganado es de: {interes_ganado}")
---
+++ actividad3/Ejercicio4.py
@@ -0,0 +1,4 @@
+dista_km = float(input("Ingrese la cantidad recorrida en kilometros: "))
+litros_consum = float(input("Ingrese la cantidad de litros consumidos: "))
+
+rendimiento = dista_km/litros_consum
