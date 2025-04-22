17b82cdd5d8af27bec0ee4ebc958dfb58d589545
steven-109 <somarenco@uamv.edu.ni>
2025-04-22 09:56:52 -0600

    Ejercicio 4

--- actividad3/Ejercicio4.py
+++ actividad3/Ejercicio4.py
@@ -2,3 +2,9 @@ dista_km = float(input("Ingrese la cantidad recorrida en kilometros: "))
 litros_consum = float(input("Ingrese la cantidad de litros consumidos: "))
 
 rendimiento = dista_km/litros_consum
+GASTO_TOTAL_COMBUS = litros_consum * 49.01
+
+print(f"La distancia recorrida en kilometros es de: {dista_km}")
+print(f"La cantidad de gasolina cosnumida en litros es de: {litros_consum}")
+print(f"El rendimiento es de: {rendimiento}")
+print(f"El gasto total en combustible es de: {GASTO_TOTAL_COMBUS}")
